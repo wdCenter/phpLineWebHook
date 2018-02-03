9b4dbe02751926142488c33ead24543568ee87a7
AD\2429175 <southwizard@hotmail.com>
2018-02-03 21:20:23 +0700

    c11

--- index.php
+++ index.php
@@ -100,7 +100,7 @@ if (!is_null($events['events'])) {
             curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
             curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
             curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-            $result = curl_exec($ch);
+            $response = curl_exec($ch);
             curl_close($ch);
 			
 			
