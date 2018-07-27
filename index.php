d8155ba5f038f5c3da767b0c8547de69dfb74cf7
sarawuthjdp <41405574+sarawuthjdp@users.noreply.github.com>
2018-07-27 11:06:36 +0700

    Update index.php

--- index.php
+++ index.php
@@ -92,7 +92,7 @@ if (!is_null($events['events'])) {
                 'messages' => 'zzz'
             ];
             $post = json_encode($data);
-            $headers = array('Content-Type: application/json');
+            $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
             $ch = curl_init($url);
             curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
